2ac6300c29c90e8584464ed4304bf8a77b09d09a
FYY <793171926@qq.com>
2020-08-18 15:15:14 +0800

    0.11

--- nga_wd_helper.js
+++ nga_wd_helper.js
@@ -6,80 +6,63 @@
 // @description       https://bbs.nga.cn/
 // @description:zh    https://bbs.nga.cn/
 // @description:zh-CN https://bbs.nga.cn/
-// @version           0.10
+// @version           0.11
 // @author            fyy99
 // @match             *://bbs.nga.cn/read.php*
 // @match             *://ngabbs.com/read.php*
 // @match             *://nga.178.com/read.php*
 // @run-at            document-end
 // @note              v0.10 初始版本：回帖批量锁隐功能完成
+// @note              v0.11 更换选中方式 & 插件不会在无管理权限的版面生效
 // @grant             none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    const commonui = window.commonui;
-    // new class
-    const block_txt_c0_chosen = document.createElement('style');
-    block_txt_c0_chosen.type = 'text/css';
-    block_txt_c0_chosen.innerHTML = ".block_txt_c0_orange{background-color:#ff7505;}";
-    document.head.appendChild(block_txt_c0_chosen);
     // click
     document.addEventListener('click', (e) => {
-        const h = commonui.parentAHerf(e.target || e.srcElement);
-        if (h && h.classList.contains("small_colored_text_btn") && h.classList.contains("stxt") && h.classList.contains("block_txt_c0") && h.classList.contains("vertmod") && h.name.startsWith('l')) {
+        if ((e.target || e.srcElement) && (e.target || e.srcElement).id.startsWith('postdate')) {
             // new button
-            if (!document.querySelector('#nga_wd_helper_pids_del')) {
-                const show_input = document.createElement('input');
-                show_input.style.width = '9em';
-                show_input.placeholder = '将在此显示锁隐结果';
-                document.querySelector("#postbbtm").appendChild(show_input);
+            if ((window.__GP.admincheck & 2) && !document.querySelector('#nga_wd_helper_pids_del')) {
                 const td = document.createElement('td');
                 td.innerHTML = '<a href="javascript:void(0)" class="cell rep txtbtnx nobr silver" title="脚本批量锁隐" id="nga_wd_helper_pids_del">回帖批量锁隐</a>';
-                td.addEventListener('click', (e) => {
-                    let all = 0;
-                    let ok = 0;
-                    for (let h of document.querySelectorAll("a.small_colored_text_btn.stxt.block_txt_c0.vertmod[name^=l].block_txt_c0_orange")) {
-                        all++;
-                        if (!(h.parentNode && h.parentNode.parentNode && h.parentNode.parentNode.parentNode && h.parentNode.parentNode.parentNode.parentNode && h.parentNode.parentNode.parentNode.parentNode.nextElementSibling && h.parentNode.parentNode.parentNode.parentNode.nextElementSibling.firstChild && h.parentNode.parentNode.parentNode.parentNode.nextElementSibling.firstChild.nodeName === 'A' && h.parentNode.parentNode.parentNode.parentNode.nextElementSibling.firstChild.id.startsWith('pid'))){
-                            show_input.value += `未知pid(失败),`;
-                            continue;
-                        }
-                        const pid_regexp = new RegExp('pid([0-9]+)Anchor').exec(h.parentNode.parentNode.parentNode.parentNode.nextElementSibling.firstChild.id);
-                        if (pid_regexp && pid_regexp[1]) {
-                            const fid = window.__CURRENT_FID;
-                            const tid = window.__CURRENT_TID;
-                            const pid = pid_regexp[1];
-                            // work!
-                            var fd = new FormData();
-                            var xr = new XMLHttpRequest();
-                            xr.onload = function (e) {
-                                const xhr = e.target;
-                                if (xhr.readyState === 4 && xhr.status === 200 && xhr.responseText.includes('操作成功')) {
-                                    show_input.value += `${pid},`;
-                                } else {
-                                    show_input.value += `${pid}(失败),`;
-                                    console.log(pid, xhr.status, xhr.responseText);
-                                }
+                td.addEventListener('click', () => {
+                    const fid = window.__CURRENT_FID;
+                    const tid = window.__CURRENT_TID;
+                    const pids = document.querySelectorAll("span[id^=postdate][class*=stxt][style*=darkred]");
+                    const ids = [];
+                    for (let pid of pids) {
+                        if (pid.parentNode && pid.parentNode.parentNode && pid.parentNode.parentNode.firstChild && pid.parentNode.parentNode.firstChild.id.startsWith('pid')){
+                            const pid_regexp = new RegExp('pid([0-9]+)Anchor').exec(pid.parentNode.parentNode.firstChild.id);
+                            if (pid_regexp && pid_regexp[1]) {
+                                ids.push(tid);
+                                ids.push(pid_regexp[1]);
                             }
-                            xr.open("POST", `/nuke.php?__lib=topic_lock&__act=set&ids=${tid}%2C${pid}&ton=0&toff=0&pon=1026&poff=0&pm=&info=&delay=&cfid=${fid}&raw=3&nga_wd_helper_pids_del=1`);
-                            xr.withCredentials = true;
-                            xr.send(fd);
                         }
                     }
+                    const fd = new FormData();
+                    const xr = new XMLHttpRequest();
+                    xr.onload = function (e) {
+                        const xhr = e.target;
+                        if (xhr.readyState === 4 && xhr.status === 200 && xhr.responseText.includes('操作成功')) {
+                            const data_regexp = new RegExp('"data":{(.*?)},"time":').exec(xhr.responseText);
+                            if (data_regexp && data_regexp[1]) {
+                                alert(data_regexp[1].replace(/,/g, '\n'));
+                            } else {
+                                console.error(ids);
+                                console.error(xhr.status, xhr.responseText);
+                            }
+                        } else {
+                            console.error(ids);
+                            console.error(xhr.status, xhr.responseText);
+                        }
+                    }
+                    xr.open("POST", `/nuke.php?__lib=topic_lock&__act=set&ids=${ids.join('%2C')}&ton=0&toff=0&pon=1026&poff=0&pm=&info=&delay=&cfid=${fid}&raw=3&nga_wd_helper_pids_del=1`);
+                    xr.withCredentials = true;
+                    xr.send(fd);
                 });
                 document.querySelector("#postbbtm > table > tbody > tr").appendChild(td);
             }
-            if (h.classList.contains("block_txt_c0_orange")) {
-                h.classList.remove('block_txt_c0_orange');
-            } else {
-                h.classList.add('block_txt_c0_orange');
-            }
-            e.returnValue = false
-            e.cancelBubble = true;
-            e.preventDefault();
-            e.stopPropagation();
-            return false;
         }
     });
 })();
