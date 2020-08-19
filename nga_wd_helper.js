51d4a8f531ee9c773c7c420fcceb5960b39dd7ec
FYY <793171926@qq.com>
2020-08-19 19:25:56 +0800

    0.15

--- nga_wd_helper.js
+++ nga_wd_helper.js
@@ -6,7 +6,7 @@
 // @description       https://bbs.nga.cn/
 // @description:zh    https://bbs.nga.cn/
 // @description:zh-CN https://bbs.nga.cn/
-// @version           0.14
+// @version           0.15
 // @author            fyy99
 // @match             *://ngabbs.com/*
 // @match             *://bbs.nga.cn/*
@@ -17,6 +17,7 @@
 // @note              v0.12 优化选中样式 & 支持帖子属性面板的其他操作
 // @note              v0.13 bugfix
 // @note              v0.14 批量加分 & 自动修改域名
+// @note              v0.15 bugfix & 显示主题样式和加精情况
 // @grant             none
 // ==/UserScript==
 
@@ -27,143 +28,176 @@
         // ngabbs.com是客户端用的域名，兼容性最好
         document.location.href = document.location.href.replace('bbs.nga.cn', 'ngabbs.com').replace('nga.178.com','ngabbs.com');
     }
-    // 读帖页面
-    if (document.location.href.includes('read.php')) {
-        // class/style
-        const nga_wd_helper_style = document.createElement('style');
-        nga_wd_helper_style.type = 'text/css';
-        nga_wd_helper_style.innerHTML = "span[id^=postdate][class*=stxt]{font-weight:bold;cursor:pointer;} span[id^=postdate][class*=stxt][style*=darkred]{color:orangered !important;text-decoration:underline;} span[id^=postdate][class*=stxt][style*=darkred]:before{content:'\\2714  '}";
-        document.head.appendChild(nga_wd_helper_style);
-        // select
-        const getSelectedPids = (tid = false) => {
-            const pids = document.querySelectorAll("span[id^=postdate][class*=stxt][style*=darkred]");
-            const ids = [];
-            for (let pid of pids) {
-                if (pid.parentNode && pid.parentNode.parentNode && pid.parentNode.parentNode.firstChild && pid.parentNode.parentNode.firstChild.id.startsWith('pid')){
-                    const pid_regexp = new RegExp('pid([0-9]+)Anchor').exec(pid.parentNode.parentNode.firstChild.id);
-                    if (pid_regexp && pid_regexp[1]) {
-                        if (tid) {
-                            ids.push(tid);
+
+    // 定时检测刷新
+    // [TODO:显示机型，显示赞/踩]
+    setInterval(function () {
+        if (document.nga_wd_helper_url == document.location.href) {
+            return;
+        }
+        document.nga_wd_helper_url = document.location.href;
+        if (document.location.href.includes('/read.php')) {
+            // 批量操作(加分)按钮 批量操作(属性)按钮
+            if ((window.__GP.admincheck & 2) && !document.querySelector('#nga_wd_helper_pids_add')) {
+                // class/style
+                const nga_wd_helper_style = document.createElement('style');
+                nga_wd_helper_style.type = 'text/css';
+                nga_wd_helper_style.innerHTML = "span[id^=postdate][class*=stxt]{font-weight:bold;cursor:pointer;} span[id^=postdate][class*=stxt][style*=darkred]{color:orangered !important;text-decoration:underline;} span[id^=postdate][class*=stxt][style*=darkred]:before{content:'\\2714  '}";
+                document.head.appendChild(nga_wd_helper_style);
+                // select
+                const getSelectedPids = (tid = false) => {
+                    const pids = document.querySelectorAll("span[id^=postdate][class*=stxt][style*=darkred]");
+                    const ids = [];
+                    for (let pid of pids) {
+                        if (pid.parentNode && pid.parentNode.parentNode && pid.parentNode.parentNode.firstChild && pid.parentNode.parentNode.firstChild.id.startsWith('pid')){
+                            const pid_regexp = new RegExp('pid([0-9]+)Anchor').exec(pid.parentNode.parentNode.firstChild.id);
+                            if (pid_regexp && pid_regexp[1]) {
+                                if (tid) {
+                                    ids.push(tid);
+                                }
+                                ids.push(pid_regexp[1]);
+                            }
                         }
-                        ids.push(pid_regexp[1]);
                     }
-                }
-            }
-            return ids;
-        };
-        // click
-        document.addEventListener('click', (e) => {
-            if ((e.target || e.srcElement) && (e.target || e.srcElement).id.startsWith('postdate')) {
-                // new button
-                if ((window.__GP.admincheck & 2) && !document.querySelector('#nga_wd_helper_pids_del')) {
-                    const td_pids_add = document.createElement('td');
-                    td_pids_add.innerHTML = '<a href="javascript:void(0)" class="cell rep txtbtnx nobr silver" title="帖子批量加分" id="nga_wd_helper_pids_add">批量操作(加分)</a>';
-                    td_pids_add.addEventListener('click', () => {
-                        const fid = window.__CURRENT_FID;
-                        const tid = window.__CURRENT_TID;
-                        const pids = getSelectedPids();
-                        if (pids.length == 0) {
-                            alert('没有任何有效的选中项目');
-                            return;
-                        }
-                        let opt = 4194304;
-                        switch (prompt("1:声望威望金钱  2:声望威望  3:声望金钱  4:声望\n其他选项视为取消操作\n该操作将发送PM", "1")) {
-                            case '1':
-                                opt = 4194304 + 4 + 2 + 1;
-                                break;
-                            case '2':
-                                opt = 4194304 + 4 + 2;
-                                break;
-                            case '3':
-                                opt = 4194304 + 4 + 1;
-                                break;
-                            case '4':
-                                opt = 4194304 + 4;
-                                break;
-                            default:
-                                alert('您已取消操作');
-                                return;
-                        }
-                        const value = prompt("请填写声望(-1500~1500)并确认\n\n加分过程中不要关闭窗口或离开本页面\n操作完成后会有弹窗提示", "15");
-                        if (!value) {
+                    return ids;
+                };
+                // #nga_wd_helper_pids_add
+                const td_pids_add = document.createElement('td');
+                td_pids_add.id = 'nga_wd_helper_pids_add';
+                td_pids_add.innerHTML = '<a href="javascript:void(0)" class="cell rep txtbtnx nobr silver" title="帖子批量加分">批量操作(加分)</a>';
+                td_pids_add.addEventListener('click', () => {
+                    const fid = window.__CURRENT_FID;
+                    const tid = window.__CURRENT_TID;
+                    const pids = getSelectedPids();
+                    if (pids.length == 0) {
+                        alert('没有任何有效的选中项目');
+                        return;
+                    }
+                    let opt;
+                    switch (prompt("1:声望威望金钱  2:声望威望  3:声望金钱  4:声望\n其他选项视为取消操作\n该操作将发送PM", "1")) {
+                        case '1':
+                            opt = 4194304 + 16 + 4 + 2 + 1;
+                            break;
+                        case '2':
+                            opt = 4194304 + 16 + 4 + 2;
+                            break;
+                        case '3':
+                            opt = 4194304 + 16 + 4 + 1;
+                            break;
+                        case '4':
+                            opt = 4194304 + 16 + 4;
+                            break;
+                        default:
                             alert('您已取消操作');
                             return;
+                    }
+                    const value = prompt("请填写声望(-1500~1500)并确认\n\n加分过程中不要关闭窗口或离开本页面\n操作完成后会有弹窗提示", "15");
+                    if (!value) {
+                        alert('您已取消操作');
+                        return;
+                    }
+                    let results = '';
+                    const mas = function (pids) {
+                        if (pids.length) {
+                            const pid = pids.shift();
+                            window.__NUKE.doRequest({
+                                u: {u: window.__API._base,
+                                    a: {__lib:"add_point_v3",__act:"add",opt:opt,fid:fid,tid:tid,pid:pid,info:'',value:value,raw:3,nga_wd_helper_pids_add:1}},
+                                f: function(d) {
+                                    const result = `pid:${pid} ${d.error ? d.error[0] : d.data[0]}`;
+                                    results += result + '\n';
+                                    console.log(result);
+                                    mas(pids);
+                                }
+                            });
+                        } else {
+                            alert(`批量操作完成\n\n${results}`);
                         }
-                        let results = '';
-                        const mas = function (pids) {
-                            if (pids.length) {
-                                const pid = pids.shift();
-                                window.__NUKE.doRequest({
-                                    u: {u: window.__API._base,
-                                        a: {__lib:"add_point_v3",__act:"add",opt:opt,fid:fid,tid:tid,pid:pid,info:'',value:value,raw:3,nga_wd_helper_pids_add:1}},
-                                    f: function(d) {
-                                        const result = `pid:${pid} ${d.error ? d.error[0] : d.data[0]}`;
-                                        results += result + '\n';
-                                        console.log(result);
-                                        mas(pids);
-                                    }
-                                });
-                            } else {
-                                alert(`批量操作完成\n\n${results}`);
-                            }
-                        }
-                        mas(pids);
-                    });
-                    document.querySelector("#postbbtm > table > tbody > tr").appendChild(td_pids_add);
-                    const td_pids_del = document.createElement('td');
-                    td_pids_del.innerHTML = '<a href="javascript:void(0)" class="cell rep txtbtnx nobr silver" title="批量设置帖子属性" id="nga_wd_helper_pids_del">批量操作(属性)</a>';
-                    td_pids_del.addEventListener('click', () => {
-                        const fid = window.__CURRENT_FID;
-                        const tid = window.__CURRENT_TID;
-                        const ids = getSelectedPids(tid);
-                        if (ids.length == 0) {
-                            alert('没有任何有效的选中项目');
+                    }
+                    mas(pids);
+                });
+                document.querySelector("#postbbtm > table > tbody > tr").appendChild(td_pids_add);
+                // #nga_wd_helper_pids_del
+                const td_pids_del = document.createElement('td');
+                td_pids_del.id = 'nga_wd_helper_pids_del';
+                td_pids_del.innerHTML = '<a href="javascript:void(0)" class="cell rep txtbtnx nobr silver" title="批量设置帖子属性">批量操作(属性)</a>';
+                td_pids_del.addEventListener('click', () => {
+                    const fid = window.__CURRENT_FID;
+                    const tid = window.__CURRENT_TID;
+                    const ids = getSelectedPids(tid);
+                    if (ids.length == 0) {
+                        alert('没有任何有效的选中项目');
+                        return;
+                    }
+                    let pon = 0;
+                    let poff = 0;
+                    switch (prompt("1:锁隐  2:锁定  3:隐藏\n4:解除锁隐  5:编辑许可  6:通过审核\n其他选项视为取消操作\n该操作不会发送PM\n\n加分过程中不要关闭窗口或离开本页面\n操作完成后会有弹窗提示", "1")) {
+                        case '1':
+                            pon = 1026;
+                            break;
+                        case '2':
+                            pon = 1024;
+                            break;
+                        case '3':
+                            pon = 2;
+                            break;
+                        case '4':
+                            poff = 1026;
+                            break;
+                        case '5':
+                            pon = 128;
+                            break;
+                        case '6':
+                            poff = 67109376;
+                            break;
+                        default:
+                            alert('您已取消操作');
                             return;
-                        }
-                        let pon = 0;
-                        let poff = 0;
-                        switch (prompt("1:锁隐  2:锁定  3:隐藏\n4:解除锁隐  5:编辑许可  6:通过审核\n其他选项视为取消操作\n该操作不会发送PM", "1")) {
-                            case '1':
-                                pon = 1026;
-                                break;
-                            case '2':
-                                pon = 1024;
-                                break;
-                            case '3':
-                                pon = 2;
-                                break;
-                            case '4':
-                                poff = 1026;
-                                break;
-                            case '5':
-                                pon = 128;
-                                break;
-                            case '6':
-                                poff = 67109376;
-                                break;
-                            default:
-                                alert('您已取消操作');
-                                return;
-                        }
-                        window.__NUKE.doRequest({
-                            u: {u: window.__API._base,
-                                a: {__lib:"topic_lock",__act:"set",ids:ids.join(','),ton:0,toff:0,pon:pon,poff:poff,pm:'',info:'',delay:'',cfid:fid,raw:3,nga_wd_helper_pids_del:1}},
-                            f: function(d) {
-                                let result = '批量操作完成\n\n';
-                                for (let i in d.error) {
-                                    result += d.error[i] + '\n';
-                                }
-                                for (let i in d.data) {
-                                    result += d.data[i] + '\n';
-                                }
-                                console.log(result);
-                                alert(result);
+                    }
+                    window.__NUKE.doRequest({
+                        u: {u: window.__API._base,
+                            a: {__lib:"topic_lock",__act:"set",ids:ids.join(','),ton:0,toff:0,pon:pon,poff:poff,pm:'',info:'',delay:'',cfid:fid,raw:3,nga_wd_helper_pids_del:1}},
+                        f: function(d) {
+                            let result = '批量操作完成\n\n';
+                            for (let i in d.error) {
+                                result += d.error[i] + '\n';
                             }
-                        });
+                            for (let i in d.data) {
+                                result += d.data[i] + '\n';
+                            }
+                            console.log(result);
+                            alert(result);
+                        }
                     });
-                    document.querySelector("#postbbtm > table > tbody > tr").appendChild(td_pids_del);
-                }
+                });
+                document.querySelector("#postbbtm > table > tbody > tr").appendChild(td_pids_del);
             }
-        });
-    }
+            // 主题样式
+            if (!document.querySelector('#nga_wd_helper_title')) {
+                const tid = window.__CURRENT_TID;
+                const title_a = document.querySelector("#m_nav a.nav_link[href^=\\/read\\.php]");
+                window.__NUKE.doRequest({
+                    u: `https://ngabbs.com/read.php?tid=${tid}&pid=0&opt=2&__output=1`,
+                    f: function (d) {
+                        if (!d.error && d.data) {
+                            console.log(d);
+                            const recommend_span = document.createElement('span');
+                            recommend_span.id = 'nga_wd_helper_title';
+                            recommend_span.style.fontSize = '0.7em';
+                            recommend_span.style.fontWeight = 'normal';
+                            recommend_span.innerHTML = ` (${d.data.__T.recommend})`;
+                            title_a.appendChild(recommend_span);
+                            const topicMiscVar = window.commonui.topicMiscVar;
+                            const topic_misc = topicMiscVar.unpack(d.data.__T.topic_misc);
+                            title_a.style.padding = '0 10px';
+                            title_a.style.color = (topic_misc._BIT1 & topicMiscVar._FONT_RED) ? '#D00' : (topic_misc._BIT1 & topicMiscVar._FONT_BLUE) ? '#06B' : (topic_misc._BIT1 & topicMiscVar._FONT_GREEN) ? '#3D9F0E' : (topic_misc._BIT1 & topicMiscVar._FONT_ORANGE) ? '#c88100' : (topic_misc._BIT1 & topicMiscVar._FONT_SILVER) ? '#888' : '#000';
+                            title_a.style.fontWeight = (topic_misc._BIT1 & topicMiscVar._FONT_B) ? 'bold' : 'normal';
+                            title_a.style.fontStyle = (topic_misc._BIT1 & topicMiscVar._FONT_I) ? 'italic' : '';
+                            title_a.style.textDecoration = (topic_misc._BIT1 & topicMiscVar._FONT_U) ? 'line-through' : '';
+                        }
+                    }
+                });
+            }
+        }
+    }, 1000);
 })();
